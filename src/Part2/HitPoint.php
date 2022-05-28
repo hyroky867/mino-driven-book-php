50482c913104e8119737bdea3c8a71b95e1e69a1
hyroky867 <handm755@gmail.com>
2022-05-28 16:19:06 +0900

    first commit

---
+++ src/Part2/HitPoint.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Part2;
+
+use InvalidArgumentException;
+
+class HitPoint
+{
+    private const MIN = 0;
+    private const MAX = 999;
+    readonly public int $value;
+
+    final public function __construct(int $value)
+    {
+        if ($value < self::MIN) {
+            throw new InvalidArgumentException(self::MIN . '以上を指定してください');
+        }
+        if (self::MAX < $value) {
+            throw new InvalidArgumentException(self::MAX . '以下を指定してください');
+        }
+        $this->value = $value;
+    }
+
+    /** ダメージを受ける */
+    public function damage(int $damageAmount): HitPoint
+    {
+        $damaged = $this->value - $damageAmount;
+        $corrected = $damaged < self::MIN ? self::MIN : $damaged;
+        return new HitPoint(value: $corrected);
+    }
+
+    /** 回復する */
+    public function recover(int $recoveryAmount): HitPoint
+    {
+        $recovered = $this->value + $recoveryAmount;
+        $corrected = self::MAX < $recovered ? self::MAX : $recovered;
+        return new HitPoint(value: $corrected);
+    }
+}
---
+++ tests/Unit/Part2/HitPointTest.php
@@ -0,0 +1,84 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Test\Unit\Part2;
+
+use App\Part2\HitPoint;
+use InvalidArgumentException;
+use PHPUnit\Framework\TestCase;
+
+class HitPointTest extends TestCase
+{
+    /** @test */
+    public function 値が最小値よりも下回る場合、例外が帰るべき(): void
+    {
+        $value = -1;
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('0以上を指定してください');
+
+        new HitPoint(value: $value);
+    }
+
+    /** @test */
+    public function 値が最小値よりも上回る場合、例外が帰るべき(): void
+    {
+        $value = 1000;
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('999以下を指定してください');
+
+        new HitPoint(value: $value);
+    }
+
+    /** @test */
+    public function 受けたダメージが持っている値よりも小さい場合、計算された値が保持されるべき(): void
+    {
+        $value = 10;
+        $point = new HitPoint(value: $value);
+
+        $damageAmount = 3;
+        $actual = $point->damage($damageAmount);
+
+        $expected = $value - $damageAmount;
+        $this->assertSame($expected, $actual->value);
+    }
+
+    /** @test */
+    public function 受けたダメージが持っている値よりも大きい場合、最小値が保持されるべき(): void
+    {
+        $value = 10;
+        $point = new HitPoint(value: $value);
+
+        $damageAmount = 100;
+        $actual = $point->damage($damageAmount);
+
+        $expected = 0;
+        $this->assertSame($expected, $actual->value);
+    }
+
+    /** @test */
+    public function 回復値との合計が最大値よりも小さい場合、計算された値が保持されるべき(): void
+    {
+        $value = 10;
+        $point = new HitPoint(value: $value);
+
+        $recoverAmount = 3;
+        $actual = $point->recover($recoverAmount);
+
+        $expected = $value + $recoverAmount;
+        $this->assertSame($expected, $actual->value);
+    }
+
+    /** @test */
+    public function 回復値が持っている値よりも大きい場合、最小値が保持されるべき(): void
+    {
+        $value = 10;
+        $point = new HitPoint(value: $value);
+
+        $damageAmount = 1000;
+        $actual = $point->recover($damageAmount);
+
+        $expected = 999;
+        $this->assertSame($expected, $actual->value);
+    }
+}
