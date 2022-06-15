c7bfdec28afd114419c43f3cfbfeac946f55ff87
hyroky867 <handm755@gmail.com>
2022-06-15 23:38:51 +0900

    add: part6.56

--- src/Part6/Chapter4/RegularRates.php
+++ src/Part6/Chapter4/RegularRates.php
@@ -6,6 +6,13 @@ namespace App\Part6\Chapter4;
 
 class RegularRates implements HotelRates
 {
+    public function busySeasonFee(): Money
+    {
+        return $this->fee()->add(
+            other: new Money(amount: 3000),
+        );
+    }
+
     public function fee(): Money
     {
         return new Money(amount: 7000);
