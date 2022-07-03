f159476bb2acde8c5bbb1f32eee2676cc1439fe1
hyroky867 <handm755@gmail.com>
2022-07-04 00:35:43 +0900

    add: part9.11

--- src/Part9/Chapter6/Equipment.php
+++ src/Part9/Chapter6/Equipment.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace App\Part9\Chapter6;
 
+use InvalidArgumentException;
+
 class Equipment
 {
     public function __construct(
@@ -12,5 +14,18 @@ class Equipment
         public readonly int $defence,
         public readonly int $magicDefence,
     ) {
+        if ($this->name === '') {
+            throw new InvalidArgumentException(message: '無効な名前');
+        }
+    }
+
+    public static function getEmpty(): self
+    {
+        return new self(
+            name: '装備なし',
+            price: 0,
+            defence: 0,
+            magicDefence: 0,
+        );
     }
 }
